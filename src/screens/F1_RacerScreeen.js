19d5dd8c670841b23b181eed21fc5e042c0996ef
Aaron <aaronalcantar19@gmail.com>
2025-10-23 22:14:41 -0700

    update F1_RacerScreeen

--- src/screens/F1_RacerScreeen.js
+++ src/screens/F1_RacerScreeen.js
@@ -11,46 +11,46 @@ const F1_RacerScreen = () => {
 
     <ScrollView style={styles.container}>
       <ImageDetail
-        title="Event_1"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="Seal Speed Circuit"
+        imageSource={require("../../assets/F1_seal/Formula 1.png")}
+        info="Join us for the annual seal grand prix racing event! Watch daring drivers zoom in Formula 1 race cars, all competing for the legendary golden fish!"
+        count={67}
       />
       <ImageDetail
-        title="Event_2"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="Seal Scramble 6000"
+        imageSource={require("../../assets/F1_seal/seal in a dirt bike 2.png")}
+        info="Join the muddy madness as we race, tumble, and flip all while riding dirt bikes!"
+        count={20}
       />
       <ImageDetail
-        title="Event_3"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="Monster Truck Mayhem"
+        imageSource={require("../../assets/F1_seal/seal going ah 2.png")}
+        info="Take the wheel of a monster truck and ride around in this epic arena of chaos! Watch them crush, crash, and flip over obstacles."
+        count={31}
       />
       <ImageDetail
-        title="Event_4"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="Seal Support Squad"
+        imageSource={require("../../assets/F1_seal/dmv seal 2.png")}
+        info="Nervous about your driving test? Join our seal squad to do practice runs and be confident hitting the road!"
+        count={7}
       />
       <ImageDetail
-        title="Event_5"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="Tokyo Street Racing"
+        imageSource={require("../../assets/F1_seal/seal street race 2.png")}
+        info="The streets are alive with speed! Take on Tokyo's toughest routes in this neon lit race track. Watch racers drift, dash, and zoom toward the finish line!"
+        count={27}
       />
       <ImageDetail
-        title="Event_6"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="FlipperDome Fury"
+        imageSource={require("../../assets/F1_seal/cool seal 2.png")}
+        info="Enter the steel sphere of chaos! Watch stunt riders race inside the cage going in loops and doing cool tricks! "
+        count={51}
       />
       <ImageDetail
-        title="Event_7"
-        imageSource={require("../../assets/halloween_seal/seal_haunted_trail_1.png")}
-        info="TBD"
-        count={0}
+        title="SealStyle Showoff"
+        imageSource={require("../../assets/F1_seal/super cool seal 2.png")}
+        info="Watch driver perform outrageous vehicle tricks and stunts! Who will pull of the coolest trick?"
+        count={95}
       />
     </ScrollView>
   );
