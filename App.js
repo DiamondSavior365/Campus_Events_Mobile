d5b046ff62d6ebf9917ad280f8ff04fdb4587932
TriIIionaires <jadon16.2007@gmail.com>
2025-11-17 23:10:28 -0800

    Add preferences feature

--- App.js
+++ App.js
@@ -5,10 +5,28 @@ import HomeScreen from "./src/screens/HomeScreen";
 import DirectoryScreen from "./src/screens/DirectoryScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
+import { SettingsProvider } from "./src/lib/supabase/hooks/useSettingsContext";
 import AuthProvider from "./src/lib/supabase/providers/AuthProvider";
 import EventListScreen from "./src/screens/EventListScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import { View, Image, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
+
+const SettingsButton = ({ navigation }) => (
+  <TouchableOpacity onPress={() => navigation.navigate("Settings_Screen")}>
+    <View style={{paddingRight: 2}}>
+      <Image
+        source={require("./assets/settings.png")}
+        style={{
+          width: 36,
+          length: 36,
+          resizeMode: "contain"
+        }}
+      />
+    </View>
+  </TouchableOpacity>
+);
 
 function RootApp() {
   return (
@@ -17,19 +35,38 @@ function RootApp() {
         initialRouteName="Home"
         screenOptions={{
           title: "",
-          headerShown: false,
+          headerShown: false
         }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings_Screen"
+          component={SettingsScreen}
+          options={{ title: "App Settings", headerShown: true }}
+        />
         <Stack.Screen
           name="Directory_Screen"
           component={DirectoryScreen}
-          options={{ headerShown: true }}
+          options={({ navigation }) => ({ 
+            headerShown: true, 
+            headerRight: () => (
+              <SettingsButton navigation={navigation}/>
+            ) 
+          })}
         />
         <Stack.Screen
           name="EventListScreen"
           component={EventListScreen}
-          options={{ headerShown: true }}
+          options={({ navigation }) => ({ 
+            headerShown: true, 
+            headerRight: () => (
+              <SettingsButton navigation={navigation}/>
+            ) 
+          })}
         />
         <Stack.Screen
           name="Login_Screen"
@@ -49,7 +86,9 @@ function RootApp() {
 export default function App() {
   return (
     <AuthProvider>
-      <RootApp />
+      <SettingsProvider>
+        <RootApp />
+      </SettingsProvider>
     </AuthProvider>
   );
 }
